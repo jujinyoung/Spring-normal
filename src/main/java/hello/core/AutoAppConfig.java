792859dd1093c69064af3d7b7e6b6250322fa230
jujinyoung <jujinyoung1838@naver.com>
2022-11-15 02:09:51 +0900

    의존관계 자동 주입 애노테이션 직접 작성 완료

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         //어디서부터 탐색할지 지정 -> 안하면 모든 자바 코드를 탐색 -> 속도 저하
-        basePackages = "hello.core",
+//        basePackages = "hello.core",
         //자동등록전 제외할 애노테이션설정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
